ed54d4b5fd25088ba7951eac04974a07d41747a0
Olena457 <olenademianenko42@gmail.com>
2025-11-24 19:47:10 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 // import Image from "next/image";
-import HeaderContainer from "@/src/components/HeaderContainer/HeaderContainer";
-// import { clsx } from 'clsx';
 
 export default function Home() {
   return (
     <div className=" mycontainer flex  items-center justify-center  dark:bg-black">
       <main className="bg-amber-800 ">
-        
-        
+   
       </main>
     </div>
   );
