8807233793643863ef3e7d4a028389e167b71528
Olena457 <olenademianenko42@gmail.com>
2025-12-04 00:22:15 +0200

    corected media styles

--- src/components/PortfolioCard/propsItems.ts
+++ src/components/PortfolioCard/propsItems.ts
@@ -6,7 +6,7 @@ export const PORTFOLIO_PROJECTS: PetProgectsData[] = [
     title: 'Weather Forecast',
     skills:
       'Next.js 16, TypeScript, Jotai, React Query, Tailwind CSS 4, Axios, Geolocation, Responsive Design',
-    role: 'Frontend Developer',
+    role: 'Frontend Developer & UI/UX Design',
     imageProgect: '/img/weather.jpg',
     description:
       'An application designed for easy checking of the weather forecast. It provides a detailed forecast for the current day and an approximate forecast for the next 7 days. Key features include flexible location search, real-time updates, <strong>Geolocation Support<strong/>, and friendly error handling. Built with Next.js 16 and TypeScript using Jotai and React Query for state management and Axios for data fetching. Fully optimized for mobile, tablet, and desktop screens.',
@@ -19,7 +19,7 @@ export const PORTFOLIO_PROJECTS: PetProgectsData[] = [
     title: 'AI Smart Chat',
     skills:
       'React, TypeScript, Mistral-tiny, Material UI, GSAP, Netlify Functions,Responsive Design',
-    role: 'Frontend Developer',
+    role: 'Frontend Developer & UI/UX Design',
     imageProgect: '/img/smart.jpg',
     description:
       'A fully responsive web application for delivering a seamless and engaging chat experience. It integrates a lightweight AI search model, Mistral-tiny, and features thoughtful UI/UX elements, including an animated greeting using GSAP. The application is built with React + TypeScript for scalability, uses Material UI for styling (with Light and Dark theme support), and leverages Netlify Functions for lightweight serverless operations (proxy requests).',
@@ -32,7 +32,7 @@ export const PORTFOLIO_PROJECTS: PetProgectsData[] = [
     title: 'AI Photo Interpreter',
     skills:
       'React, Material UI, TensorFlow.js, Text-to-Speech API, AI/Machine Learning, Responsive Design',
-    role: 'Frontend Developer',
+    role: 'Frontend Developer & UI/UX Design',
     imageProgect: '/img/reader.jpg',
     description:
       'A React-based application that analyzes images using a pre-trained AI model (@tensorflow-models/mobilenet). It generates a textual description, which is then converted to speech using the Text-to-Speech API. The app provides fast response time, a simple UI styled with Material UI, and supports interface language switching (Ukrainian and English). It is fully responsive and optimized for all devices.',
@@ -45,7 +45,7 @@ export const PORTFOLIO_PROJECTS: PetProgectsData[] = [
     title: 'Wacanda Flow',
     skills:
       'React, GSAP, ScrollTrigger, HTML, CSS/Tailwind, JavaScript, Animations',
-    role: 'Frontend Developer',
+    role: 'Frontend Developer & UI/UX Design',
     imageProgect: '/img/wacanda.jpg',
     description:
       'A web application that immerses users in a dynamic, animated atmosphere where every scroll becomes part of the story. The app uses the GSAP animation library and its ScrollTrigger plugin to synchronize scroll behavior with smooth, high-performance animations, including distortion and deformation effects. It features a fully responsive, immersive design where the video container dynamically scales up or down.',
@@ -70,7 +70,7 @@ export const PORTFOLIO_PROJECTS: PetProgectsData[] = [
     title: 'Food & Drinks - Cookbook',
     skills:
       'React,Vite, Redux, TypeScript, Tailwind, Axios, React Router, LocalStorage',
-    role: 'Frontend Developer',
+    role: 'Frontend Developer & UI/UX Design',
     imageProgect: '/img/food.jpg',
     description:
       'A stylish and intuitive web app for discovering recipes for meals and drinks. Users can search recipes, explore detailed nutrient content for each dish, and save favorite recipes directly in the browser using LocalStorage. It is built using TypeScript with Redux Toolkit for state management and Axios for API integration, ensuring fast navigation and responsive design.',
@@ -83,7 +83,7 @@ export const PORTFOLIO_PROJECTS: PetProgectsData[] = [
     title: 'AIDE',
     skills:
       'React + Vite, Redux Toolkit, Material UI, Framer Motion, React Slick, TypeScript, 3D Animation',
-    role: 'Frontend Developer',
+    role: 'Frontend Developer & UI/UX Design',
     imageProgect: '/img/aide.jpg',
     description:
       'A modern React-based landing page built to inspire developers and showcase modern UI/UX techniques. Key features include a 3D animated hero section, interactive animated cards, a dynamic form component, and a responsive slider (React Slick). The application uses Redux Toolkit for state management and Framer Motion for animations. It also features a section with curated links to official documentation and useful AI tools.',
@@ -93,9 +93,8 @@ export const PORTFOLIO_PROJECTS: PetProgectsData[] = [
   {
     id: 8,
     title: 'Interspace - Landing Page',
-    skills:
-      'React, TypeScript, SCSS, AOS, HTML, JavaScript, Responsive Design',
-    role: 'Frontend Developer',
+    skills: 'React, TypeScript, SCSS, AOS, HTML, JavaScript, Responsive Design',
+    role: 'Frontend Developer & UI/UX Design',
     imageProgect: '/img/inter.jpg',
     description:
       'A modern and responsive landing page template for creative design agencies. It features a clean, minimalistic UI with smooth scroll animations powered by AOS. The structure includes Header (with active navigation), Main (covering features, team, awards), and Footer. Built using a modern frontend stack with TypeScript for type-safe code and SASS for modular styles.',
@@ -107,7 +106,7 @@ export const PORTFOLIO_PROJECTS: PetProgectsData[] = [
     title: 'Quizlet Game',
     skills:
       'React, Redux, Firebase, HTML, CSS, JavaScript, API Integration, Responsive Design',
-    role: 'Frontend Developer',
+    role: 'Frontend Developer & UI/UX Design',
     imageProgect: '/img/quizlet.jpg',
     description:
       'An interactive and educational game where users can participate in quizzes and create their own quiz questions. Core features include: full user authentication via Firebase, state management with Redux, category selection, result tracking, and API integration for random bonus facts. The application is fully responsive and covers main pages, game flow pages, and user profile management.',
