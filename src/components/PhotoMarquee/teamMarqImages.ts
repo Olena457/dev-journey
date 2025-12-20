24a40fb71911989d57f3f27bb32d8c4a6daad285
Olena457 <olenademianenko42@gmail.com>
2025-12-20 13:10:27 +0200

    added photo and corected speed

--- src/components/PhotoMarquee/teamMarqImages.ts
+++ src/components/PhotoMarquee/teamMarqImages.ts
@@ -2,16 +2,16 @@
 import { MarqueeItem } from '../../types/types';
 
 export const MARQUEE_IMAGES: MarqueeItem[] = [
-  { id: 1, imageProgect: '/img/weather.jpg' },
   { id: 2, imageProgect: '/img/smart.jpg' },
   { id: 3, imageProgect: '/img/reader.jpg' },
+  { id: 1, imageProgect: '/img/weather.jpg' },
+  { id: 11, imageProgect: '/img/bit.jpg' },
   { id: 4, imageProgect: '/img/wacanda.jpg' },
-  { id: 5, imageProgect: '/img/travel.jpg' },
   { id: 6, imageProgect: '/img/food.jpg' },
-  { id: 7, imageProgect: '/img/aide.jpg' },
-  { id: 8, imageProgect: '/img/inter.jpg' },
-  { id: 9, imageProgect: '/img/quizlet.jpg' },
-  { id: 10, imageProgect: '/img/pet.jpg' },
-  { id: 11, imageProgect: '/img/event.jpg' },
-  { id: 12, imageProgect: '/img/learn.jpg' },
+  { id: 7, imageProgect: '/img/do.jpg' },
+  { id: 8, imageProgect: '/img/dez.jpg' },
+  { id: 9, imageProgect: '/img/qui.jpg' },
+  { id: 10, imageProgect: '/img/ins.jpg' },
+  { id: 12, imageProgect: '/img/job.jpg' },
+  { id: 5, imageProgect: '/img/cat.jpg' },
 ];
