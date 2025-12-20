14e6941c9f787899656ccd95bbd5852bad735b80
Olena457 <olenademianenko42@gmail.com>
2025-12-20 18:08:43 +0200

    Update HeroComponent.tsx

--- src/components/HeroComponent/HeroComponent.tsx
+++ src/components/HeroComponent/HeroComponent.tsx
@@ -2,6 +2,33 @@
 
 import styles from './HeroComponent.module.scss';
 import LetterAnimation from '../LetteAnimation/LetterAnimation';
+import  GitIcon  from '@/public/icons/GitIcon';
+import TelegramIcon from '@/public/icons/TelegramIcon';
+import EmailIcon from '@/public/icons/EmailIcon';
+import LinkedinIcon from '@/public/icons/LinkedinIcon';
+
+const socialLinks = [
+  {
+    id: 1,
+    href: 'https://github.com/Olena457', 
+    icon: <GitIcon />,
+  },
+  {
+    id: 2,
+    href: 'https://t.me/lenOK890',
+    icon: <TelegramIcon />,
+  },
+  {
+    id: 3,
+    href: 'mailto:olenademianenko42@gmail.com',
+    icon: <EmailIcon />,
+  },
+  {
+    id: 4,
+    href: 'https://www.linkedin.com/in/olena-demianenko/', // уточніть повне посилання
+    icon: <LinkedinIcon />,
+  },
+];
 
 export const HeroComponet: React.FC = () => {
   return (
@@ -16,6 +43,20 @@ export const HeroComponet: React.FC = () => {
               curiosity, continuous learning, and effective development
               methodologies.
             </p>
+
+            <div className={styles.socialWrapper}>
+              {socialLinks.map(link => (
+                <a
+                  key={link.id}
+                  href={link.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.socialLink}
+                >
+                  {link.icon}
+                </a>
+              ))}
+            </div>
           </div>
 
           <div className={styles.animationBlock}>
