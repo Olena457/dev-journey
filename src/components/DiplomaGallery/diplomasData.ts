427211efe7c9a2a35595335eeba1f3b77231fcf3
Olena457 <olenademianenko42@gmail.com>
2025-12-22 23:49:33 +0200

    corected styles section sertificates

--- src/components/DiplomaGallery/diplomasData.ts
+++ src/components/DiplomaGallery/diplomasData.ts
@@ -1,14 +1,18 @@
 import {Diploma} from '@/src/types/types'
 
 export const diplomas: Diploma[] = [
-  { id: 1, title: 'GoIT Certificate', image: '/certifs/goit.jpg' },
-  { id: 2, title: 'React Certificate', image: '/certifs/react-sert.jpg' },
+  {
+    id: 1,
+    title: ' Full-Stack Certificate',
+    image: '/certifs/goit.jpg',
+  },
+  { id: 2, title: 'React,Redux Certificate', image: '/certifs/react-sert.jpg' },
   { id: 4, title: 'English Intermediate', image: '/certifs/english.jpg' },
-  { id: 8, title: 'Netai Course', image: '/certifs/netai.jpg' },
-  { id: 9, title: 'SkillUp', image: '/certifs/skill.jpg' },
+  { id: 8, title: 'Netai Course AI', image: '/certifs/netai.jpg' },
+  { id: 9, title: 'TypeScript Mastery', image: '/certifs/skill.jpg' },
   { id: 7, title: 'Photoshop for Developers', image: '/certifs/photoshop.jpg' },
-  { id: 5, title: 'English Advanced', image: '/certifs/english-2.jpg' },
+  { id: 5, title: 'Business English', image: '/certifs/english-2.jpg' },
   { id: 10, title: 'Cyber Security Basics', image: '/certifs/cuber.jpg' },
-  { id: 3, title: 'JavaScript Certificate', image: '/certifs/sckript.jpg' },
-  { id: 6, title: 'Git Mastery', image: '/certifs/git.jpg' },
+  { id: 3, title: 'TypeScript Programming', image: '/certifs/sckript.jpg' },
+  { id: 6, title: 'Git Development', image: '/certifs/git.jpg' },
 ];
