9ae3b6b5a58c7161c0588c1add29af26a27ca474
Olena457 <olenademianenko42@gmail.com>
2025-12-23 00:16:32 +0200

    added container styles

--- src/components/DiplomaGallery/DiplomaGallery.tsx
+++ src/components/DiplomaGallery/DiplomaGallery.tsx
@@ -5,26 +5,28 @@ import styles from './DiplomaGallery.module.scss';
 
 export const DiplomaGallery: React.FC = () => {
   return (
-    <div  id="sertificates"    className={styles.section}>
-      <h2 className={styles.title}>Services</h2>
+    <div id="sertificates" className={styles.section}>
+      <section id="services" className={styles.sertificates}>
+        <h2 className={styles.title}>Sertificates</h2>
 
-      <div className={styles.container}>
-        {diplomas.map((item: Diploma) => (
-          <div key={item.id} className={styles.hexagon}>
-            <div className={styles.hexInner}>
-              <Image
-                src={item.image}
-                alt={item.title}
-                width={350}
-                height={350}
-                className={styles.img}
-              />
-              <div className={styles.borderLine}></div>
+        <div className={styles.container}>
+          {diplomas.map((item: Diploma) => (
+            <div key={item.id} className={styles.hexagon}>
+              <div className={styles.hexInner}>
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  width={350}
+                  height={350}
+                  className={styles.img}
+                />
+                <div className={styles.borderLine}></div>
+              </div>
+              <span className={styles.label}>{item.title}</span>
             </div>
-            <span className={styles.label}>{item.title}</span>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
